3c7aeb946104dba8ce86af8935001770aa734f80
Segfah <42corozco@gmail.com>
2023-08-24 02:47:29 +0200

    k3d instalacion

---
+++ p3/README.md
@@ -0,0 +1,4 @@
+## install envirenement
+```
+make install
+```
---
+++ p3/scripts/docker_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Vérifier si Docker est déjà installé
+if command -v docker &>/dev/null; then
+	echo "Docker est déjà installé. Version : $(docker --version)"
+else
+	echo "Installation de Docker..."
+	# Commandes d'installation
+	sudo apt update
+	sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+	sudo apt update
+	sudo apt install -y docker-ce
+	sudo systemctl start docker
+	sudo systemctl enable docker
+	sudo usermod -aG docker $USER
+	echo "Docker a été installé avec succès.... c'est mieux si tu demarres ton ordi"
+fi
