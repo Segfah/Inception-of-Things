1777a1adc0cb0577be84d86f0893928f0105b1b2
Segfah <42corozco@gmail.com>
2023-08-28 01:40:49 +0200

    no entiendo

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,2 +1,5 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+argocd admin initial-password -n argocd
