ac62f783673a0e6f6ac1d6003c45af5aa0b9f11c
42corozco <42corozco@gmail.com>
2023-08-28 13:12:10 +0200

    fix makefile, setup.sh

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,5 +1,5 @@
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl create namespace argocd
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
 argocd admin initial-password -n argocd
