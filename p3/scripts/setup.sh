df137b43b80ef5d7fa94d588ce4828c1f1e062da
42corozco <42corozco@gmail.com>
2023-08-29 12:36:10 +0200

    add port-forward argocd

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,5 +1,7 @@
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
 argocd admin initial-password -n argocd
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
