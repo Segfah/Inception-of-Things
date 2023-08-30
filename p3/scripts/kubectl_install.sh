bbbe4b6aa8f5246ad98ae19a00dcd277b41848c9
louisabricot <louisabricot@protonmail.com>
2023-08-30 16:17:17 +0200

    Trying to automate the installation

--- p3/scripts/kubectl_install.sh
+++ p3/scripts/kubectl_install.sh
@@ -3,5 +3,6 @@ sudo apt update
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 # Installer kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 echo "kubectl a été installé."
 
--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,13 +1,15 @@
 kubectl create namespace argocd
+kubectl create namespace dev
 # il faut creer le dev? ou la config le fait? 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
-argocd admin initial-password -n argocd
 
 # para esperar a que los pods de argocd esten preparados. 
 kubectl wait -n argocd --for=condition=Ready pods --all --timeout=-1s
 
 kubectl apply -n argocd -f ./conf/conf.yaml
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+argocd admin initial-password -n argocd
+
+#kubectl port-forward svc/argocd-server -n argocd 8080:443
