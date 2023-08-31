e6981e765037faae61c36e3e0b1dbfe508ef66ff
Segfah <42corozco@gmail.com>
2023-08-31 23:28:07 +0200

    rename gitlab.conf gitlab.yaml, update cleanup.sh, add kubectl apply in setup

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -3,10 +3,12 @@ kubectl create namespace gitlab
 # il faut creer le dev? ou la config le fait? 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+kubectl apply -n gitlab -f ./conf/gitlab.yaml
 
 
 # para esperar a que los pods de argocd esten preparados. 
 kubectl wait -n argocd --for=condition=Ready pods --all --timeout=-1s
+kubectl wait -n gitlab --for=condition=Ready pods --all --timeout=-1s
 
 #kubectl apply -n argocd -f ./conf/conf.yaml
 
