119bbdc5ce1d94d097b642a3cc636254762e9654
Segfah <42corozco@gmail.com>
2023-08-31 22:42:17 +0200

    update cleanup.sh p3

--- p3/scripts/cleanup.sh
+++ p3/scripts/cleanup.sh
@@ -1,13 +1,2 @@
-kubectl delete --all ingresses
-kubectl delete --all deployments
-kubectl delete --all pods
-kubectl delete --all services
-kubectl delete --all pvc
-
-kubectl delete ingresses --all --all-namespaces
-kubectl delete deployment --all --all-namespaces
-kubectl delete service --all --all-namespaces
-kubectl delete pod --all --all-namespaces
-kubectl delete replicaset --all --all-namespaces
-kubectl delete statefulset --all --all-namespaces
-kubectl delete jobs --all -n kube-system
+kubectl delete ns dev
+kubectl delete ns argocd
